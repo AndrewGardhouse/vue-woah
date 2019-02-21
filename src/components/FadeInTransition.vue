25814e0c6658ed5b1965ff8a9145996acca83646
Andrew Gardhouse <andrewgardhouse@macbook-air.gateway.2wire.net>
2019-02-21 16:56:58 -0500

    Adds FadeInTransition

---
+++ src/components/FadeInTransition.vue
@@ -0,0 +1,55 @@
+<template>
+  <component :is="type"
+             :tag="tag"
+             :class="wrapperClass"
+             enter-active-class="fadeIn-animation"
+             leave-active-class="fadeIn-animation-reverse"
+             move-class="smooth-move"
+             v-bind="$attrs"
+             v-on="hooks">
+      <slot></slot>
+  </component>
+</template>
+<script>
+import transitionMixin from '@/mixins/transitionMixin'
+
+export default {
+  mixins: [transitionMixin],
+  props: {
+    duration: {
+      type: Number,
+      default: 1000,
+    },
+  },
+};
+</script>
+
+<style lang="css">
+@keyframes fadeIn {
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+}
+
+.woah {
+  animation-fill-mode: both;
+}
+
+.fadeIn-animation,
+.fadeIn-animation-reverse {
+  animation-timing-function: linear;
+  transform-origin: bottom center;
+  animation-name: fadeIn;
+}
+
+.fadeIn-animation-reverse {
+  animation-direction: reverse;
+}
+
+.smooth-move {
+  transition: transform 1s linear;
+}
+</style>
