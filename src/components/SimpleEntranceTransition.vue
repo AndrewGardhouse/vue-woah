d1077157323ba9dbeaf74b9c2078078a485f5c19
Andrew Gardhouse <andrewgardhouse@macbook-air.gateway.2wire.net>
2019-02-21 16:20:40 -0500

    Adds SimpleEntranceTransition

---
+++ src/components/SimpleEntranceTransition.vue
@@ -0,0 +1,74 @@
+<template>
+  <component :is="type"
+             :tag="tag"
+             :class="wrapperClass"
+             enter-active-class="simpleEntrance-animation"
+             leave-active-class="simpleEntrance-animation-reverse"
+             move-class="smooth-move"
+             v-bind="$attrs"
+             v-on="hooks">
+      <slot></slot>
+  </component>
+</template>
+<script>
+import transitionMixin from '@/mixins/transitionMixin'
+
+export default {
+  mixins: [transitionMixin],
+  props: {
+    duration: {
+      type: Number,
+      default: 10000,
+    },
+  },
+};
+</script>
+
+<style lang="css">
+@keyframes simpleEntrance {
+  0% {
+    opacity: 0%;
+    transform: translate3d(-2000px,500px,0) scale(10);
+  }
+  1% {
+    opacity: 100%
+  }
+  40% {
+    transform: translate3d(2000px,0px,0) scale(5);
+  }
+  50% {
+    transform: translate3d(-2000px,-100px,0) scale(3);
+  }
+  60% {
+    transform: translate3d(2000px,100px,0) scale(2);
+  }
+  70% {
+    transform: translate3d(-2000px,400px,0) scale(.1);
+  }
+  75% {
+    transform: translate3d(0px,0px,0) scale(.1);
+  }
+  80% {
+    transform:  rotate(360deg) translateX(150px) rotate(-360deg)
+  }
+}
+
+.woah {
+  animation-fill-mode: both;
+}
+
+.simpleEntrance-animation,
+.simpleEntrance-animation-reverse {
+  animation-timing-function: linear;
+  transform-origin: bottom center;
+  animation-name: simpleEntrance;
+}
+
+.simpleEntrance-animation-reverse {
+  animation-direction: reverse;
+}
+
+.smooth-move {
+  transition: transform 1s linear;
+}
+</style>
